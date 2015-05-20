02234b32dd6f76a0315c5d914fa130fb16191723
MasterX1582 <davy1582@msn.com>
2015-05-20 19:50:15 +0200

    New Theme + completion Dice Account
    New Theme + completion Dice Account

--- content/ajax/generate_address.php
+++ content/ajax/generate_address.php
@@ -18,7 +18,7 @@ include '../../inc/functions.php';
 if (empty($_GET['_unique']) || mysql_num_rows(mysql_query("SELECT `id` FROM `players` WHERE `hash`='".prot($_GET['_unique'])."' LIMIT 1"))==0) exit();
 $player=mysql_fetch_array(mysql_query("SELECT `id` FROM `players` WHERE `hash`='".prot($_GET['_unique'])."' LIMIT 1"));
 
-$new_addr=$wallet->getnewaddress();
+$new_addr=$wallet->getnewaddress($DiceAccount);
 mysql_query("INSERT INTO `deposits` (`player_id`,`address`) VALUES ($player[id],'$new_addr')");
 
 echo json_encode(array('confirmed'=>$new_addr));
