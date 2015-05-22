378f394dc033469839db397d29c0cf417dbc135d
MasterX1582 <davy1582@msn.com>
2015-05-22 14:11:44 +0200

    correction
    changed $rpcssl into $rpcproto

--- install/driver_test_call.php
+++ install/driver_test_call.php
@@ -9,14 +9,14 @@
 if(isset($_POST['rpcssl']) && 
    $_POST['rpcssl'] !== 'https') 
 {
-    $rpcssl="http";
+    $rpcproto="http";
 }
 else
 {
-    $rpcssl="https";
+    $rpcproto="https";
 }  
 include 'driver_test.php';
-$test=new jsonRPCClient($rpcssl.'://'.$_GET['w_user'].':'.$_GET['w_pass'].'@'.$_GET['w_host'].':'.$_GET['w_port'].'/');
+$test=new jsonRPCClient($rpcproto.'://'.$_GET['w_user'].':'.$_GET['w_pass'].'@'.$_GET['w_host'].':'.$_GET['w_port'].'/');
 @$test_call=$test->getbalance();
 
 echo json_encode(array('result'=>$test_call));
